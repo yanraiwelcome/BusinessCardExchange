2a814213a837103335f5584469961a4d87f636ca
Prabin <silwalprabin@gmail.com>
2016-07-22 06:21:17 +0545

    New NFC path fixed

--- app/src/main/java/com/project/businesscardexchange.yan/businesscardexchange/ui/transfer/fragment/OnSelectItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.project.businesscardexchange.yan.businesscardexchange.ui.transfer.fragment;
-
-
-/**
- * Created by CrazyCoder on 2015/9/17.
- */
-public interface OnSelectItemClickListener {
-    public void onItemClicked(int type);
-}
